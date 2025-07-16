38b4046c42a66ae8b80df4ef273ecd813d5b080a
Farah Amalia <140934377+farahamaliaa@users.noreply.github.com>
2025-07-16 14:12:56 +0700

    Update ClassStudentImport.php

--- app/Imports/ClassStudentImport.php
+++ app/Imports/ClassStudentImport.php
@@ -52,7 +52,7 @@ class ClassStudentImport implements ToModel, WithHeadingRow, WithEvents
 
             $studentId = "";
             $user->assignRole(RoleEnum::STUDENT->value);
-            $birthDate = $row['tanggal_lahir'] ? Carbon::createFromFormat('Y-m-d', $row['tanggal_lahir'])->format('Y-m-d') : null;
+            $birthDate = $row['tanggal_lahir'] ? Carbon::createFromFormat('d/m/Y', $row['tanggal_lahir'])->format('Y-m-d') : null;
 
             $data = [
                 'user_id' => $user->id,
