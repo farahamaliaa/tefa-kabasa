ed669f6cdf4ed7ef7c177f29582ed66936638bbb
Farah Amalia <140934377+farahamaliaa@users.noreply.github.com>
2025-10-16 13:52:02 +0700

    Update StudentAttendanceSheet.php

--- app/Exports/StudentAttendanceSheet.php
+++ app/Exports/StudentAttendanceSheet.php
@@ -45,11 +45,11 @@ class StudentAttendanceSheet implements FromCollection, WithTitle, WithStyles, S
 
         $data = collect([
             ['RINGKASAN KEHADIRAN'],
-            ['Hadir', $statusCounts['hadir'] ?? 0],
-            ['Telat', $statusCounts['telat'] ?? 0],
-            ['Sakit', $statusCounts['sakit'] ?? 0],
-            ['Izin', $statusCounts['izin'] ?? 0],
-            ['Alpha', $statusCounts['alpha'] ?? 0],
+            ['Hadir', $statusCounts['hadir'] ?? '0'],
+            ['Telat', $statusCounts['telat'] ?? '0'],
+            ['Sakit', $statusCounts['sakit'] ?? '0'],
+            ['Izin', $statusCounts['izin'] ?? '0'],
+            ['Alpha', $statusCounts['alpha'] ?? '0'],
             [''],
             ['DETAIL KEHADIRAN'],
             ['No', 'Tanggal', 'Nama', 'Kelas', 'Keterangan', 'Masuk', 'Pulang', 'Poin']
